ba14fb38b7e95ca34fae48c9caa40858922bce83
faakit <andrealtosan@gmail.com>
2023-10-13 10:34:44 -0300

    fix: update deps and fix error validation

--- src/app/errors/FieldValidationError.ts
+++ src/app/errors/FieldValidationError.ts
@@ -1,11 +1,13 @@
-import { ValidationError } from 'joi';
+import { ValidationError, ValidationErrorItem } from 'joi';
 
 export class FieldValidationError extends ValidationError {
   constructor(_errors: { field: string; message: string }[]) {
-    const errors = _errors.map(({ field, message }) => ({
+    const errors: ValidationErrorItem[] = _errors.map(({ field, message }) => ({
       field,
       context: { label: field },
       message,
+      path: [field],
+      type: 'any',
     }));
 
     super('MISSING_PARAMS', errors, errors);
